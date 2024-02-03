3e93cb028c1830ff06e5212f26e5e6f72473535e
Softkorner <softKorner.ai@gmail.com>
2024-02-03 11:36:52 +0530

    Implemented Generics Methods

---
+++ Generics_3/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Main1
+    {
+        static void Main()
+        {
+            DeleteArray deleteInt = new DeleteArray();
+
+            int[] integer = { 1, 2, 3, 4, 5, 6 };
+            string[] str = { "Rohan", "Amey", "Sahil", "Aman", "Hemant" };
+            char[] character = { 'a', 'b', 'c', 'd', 'e', 'f' };
+            double[] doubles = {12.87, 34.5, 65.7, 2.76, 54.9, 12.2 };
+
+            deleteInt.DeleteMethod(integer);
+            deleteInt.DeleteMethod(str);
+            deleteInt.DeleteMethod(character);
+            deleteInt.DeleteMethod(doubles);
+        }
+    }
+}
---
+++ Generics_3/Generics.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class DeleteArray
+    {
+        public void DeleteMethod<T>(T[] arr)        //Method to delete a Integer element from list
+        {
+            foreach (T ele in arr)                  //Printing The Array
+            {
+                Console.Write(ele + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the index of element you want to delete: ");
+            int element = Convert.ToInt32(Console.ReadLine());
+            
+
+            List<T> ints = new List<T>(arr);        //Converting the array into a list
+            ints.RemoveAt(element);                   //removing the element from list
+                                                    //converting back to Array
+            T[] newArray = ints.ToArray();
+
+            foreach (T ele in newArray)             //Printing the new array
+            {
+                Console.Write(ele + " ");
+            }
+
+            Console.WriteLine();
+
+
+
+        }
+
+       
+
+    }
+
+
+
+}
