dc87bf5512c4e22c1b4ce21905e8391ce34f40ae
Softkorner <softKorner.ai@gmail.com>
2024-02-03 09:49:58 +0530

    Generics Assignment 1

---
+++ Generics 1/DeleteMethod.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class DeleteArray
+    {
+        public void DeleteMethod(int[] arr)          //Method to delete a Integer element from list
+        {
+            foreach(int ele in arr)                  //Printing The Array
+            {
+                Console.Write(ele + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Enter the element you want to delete: ");
+            int element = Convert.ToInt32(Console.ReadLine());
+
+            List<int> ints = new List<int>(arr);   //Converting the array into a list
+            ints.Remove(element);                  //removing the element from list
+
+            int[] newArray = ints.ToArray();
+
+            foreach(int ele in newArray)           //Printing the new array
+            {
+                Console.Write(ele + " ");
+            }
+
+            Console.WriteLine() ;   
+
+            
+
+        }
+
+        public void DeleteMethod(string[] arr)    //Method to delete a string element from list
+        {
+
+            foreach(string ele in arr)             //Printing the Array
+            {
+                Console.Write(ele + " ");
+            }
+
+            Console.WriteLine() ;
+            
+            Console.WriteLine("Enter the element you want to delete: ");
+            string element = Console.ReadLine();
+
+            List<string> str = new List<string>(arr);   //Converting the array into a list
+            str.Remove(element);                  //removing the element from list
+
+            string[] newArray = str.ToArray();
+
+            foreach (string ele in newArray)
+            {
+                Console.Write(ele + " ");
+            }
+
+            Console.WriteLine();
+
+            
+        }
+
+        public void DeleteMethod(char[] arr)
+        {
+
+            
+            foreach (char ele in arr) //Printing the Array
+            {
+                Console.Write(ele + " ");
+            }
+
+            Console.WriteLine();
+            
+            Console.WriteLine("Enter the element you want to delete: ");    //Method to delete a character element from list
+            char element = Convert.ToChar(Console.ReadLine());
+
+            List<char> character = new List<char>(arr);   //Converting the array into a list
+            character.Remove(element);                  //removing the element from list
+
+            char[] newArray = character.ToArray();
+
+            foreach (char ele in newArray)
+            {
+                Console.Write(ele + " ");
+            }
+
+            Console.WriteLine();
+            
+        }
+
+    }
+
+
+   
+}
---
+++ Generics 1/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Main1
+    {
+        static void Main()
+        {
+            DeleteArray deleteInt = new DeleteArray();
+
+            int[] integer = { 1, 2, 3 ,4 ,5 ,6};
+            string[] str = { "Rohan" ,"Amey" ,"Sahil" ,"Aman" ,"Hemant"};
+            char[] character = { 'a', 'b', 'c' ,'d' ,'e' ,'f'};
+
+            deleteInt.DeleteMethod(integer);
+            deleteInt.DeleteMethod(str);
+            deleteInt.DeleteMethod(character);
+        }
+    }
+}
