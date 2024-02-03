e4258e30f1578c2726fea07cc33aac7891e3fe99
Softkorner <softKorner.ai@gmail.com>
2024-02-03 11:43:59 +0530

    Minimum of three Numbers

---
+++ Generics 4 5 6/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_5
+{
+    public class Minnumber
+    {
+        public void Minnum(int[] arr)
+        {
+            int min = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+
+            }
+        }
+
+        public void Minnum(float[] arr)
+        {
+            float min = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+
+            }
+
+        }
+
+
+
+
+    }
+
+    public class MinString<T> where T : IComparable<T>
+    {
+        public void Minnum(T[] arr)
+        {
+            T min = arr[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i].CompareTo(min) < 0)
+                {
+                    min = arr[i];
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Generics 4 5 6/MainTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_5
+{
+    internal class MainTest
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 5, 6, 8, 4 };
+            float[] arr1 = { 12.4f, 5.4f, 5.54f, 7.75f, 89.8f };
+            string[] arr2 = { "amey", "Hemant", "Rohan", "Rahul" };
+            Minnumber minNumber = new Minnumber();
+            minNumber.Minnum(arr);
+            minNumber.Minnum(arr1);
+
+            MinString<string> minNumber1 = new MinString<string>();
+            minNumber1.Minnum(arr2);
+
+
+        }
+    }
+}
